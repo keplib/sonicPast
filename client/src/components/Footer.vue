bc1b7d859785ae57ed2c232fa96edf04450fdbdd
Lucas de Oliveira Santos <catadorfgh@outlook.com>
2022-09-29 12:57:37 +0200

    test push

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,24 +1,11 @@
 <template>
-    
-    <footer>
-        <p>Barcelona, 2022. </p>
-    </footer>
+  <footer>
+    <p>Barcelona, 2022.</p>
+  </footer>
 </template>
 
-
-
-
-
 <script setup>
-
+//test comment
 </script>
 
-
-
-<style>
-
-
-
-</style>
-
-
+<style></style>
