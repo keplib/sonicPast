00d05f0885892687f4fb0a0cbaf0f02ae7ae8cf4
Lucas de Oliveira Santos <catadorfgh@outlook.com>
2022-09-29 12:45:29 +0200

    no merge confisdtnsad

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,12 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { createPinia } from "pinia";
 
 const pinia = createPinia();
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(pinia)
-app.use(router)
+app.use(pinia);
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
