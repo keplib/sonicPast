ec08ea2a199efeb87f626c49c9a574c81c286312
Lucas de Oliveira Santos <catadorfgh@outlook.com>
2022-10-04 11:37:38 +0200

    added some tests

--- client/test/app.test.js
+++ client/test/app.test.js
@@ -1,39 +1,9 @@
 // import Playlist from "../src/views/Playlist.vue";
 import { describe, test } from "vitest";
 import supabase from "../src/db/client";
+import axios from "axios";
 
 const { expect } = require("chai");
-const toggleFav = async (item, fav) => {
-  try {
-    const { data, error } = await supabase
-      .from("playlists_db")
-      .update({ favourite: !fav })
-      .match({ id: item });
-
-    const getData = async () => {
-      try {
-        const { data, error } = await supabase.from("playlists_db").select();
-
-        userPlaylists.value = data;
-        console.log(userPlaylists.value);
-      } catch (error) {
-        console.log("error getting favourite data from supabase", error);
-      }
-    };
-
-    getData();
-  } catch (error) {
-    console.log("error toggling favourite", error);
-  }
-};
-
-describe("toggleFav", () => {
-  test("expect to toggle favourites", () => {
-    toggleFav(1, true).then((response) => {
-      console.log(response);
-    });
-  });
-});
 
 describe("supabase", () => {
   test("expect response from database", () => {
@@ -41,7 +11,26 @@ describe("supabase", () => {
       .from("playlists_db")
       .select()
       .then((response) => {
-        expect(response.data).not(null);
+        expect(response.data).toString();
       });
+  });
+});
+const getChart = async () => {
+  const options = {
+    method: "GET",
+    url: "http://localhost:3000/api/chart",
+    params: { date: "1997-10-06" },
+  };
+
+  try {
+    const response = await axios.request(options);
+    return response.data.songs.slice(0, 10);
+  } catch (error) {
+    console.log(error);
+  }
+};
+describe("Chart Data", () => {
+  test("should get charts from api", async () => {
+    expect(await getChart()).to.be.a("array");
   });
 });
