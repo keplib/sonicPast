991d1105c9f74986c54e7888b100a60aa7b91498
Lucas de Oliveira Santos <catadorfgh@outlook.com>
2022-10-04 16:45:16 +0200

    fixed height

--- client/src/components/Images.vue
+++ client/src/components/Images.vue
@@ -1,24 +1,32 @@
 <template>
   <div id="info-container" class="flex flex-row h-full w-full">
     <div>
-    <h3
-      v-if="rank && rank < 10"
-      class="text-green text-4xl tracking-wider font-jetbrains italic absolute"
-    >
-      0{{ rank }}.
-    </h3>
-    <h3 v-else
-      class="text-green text-4xl tracking-wider font-jetbrains italic absolute"
-    >
-      {{ rank }}.
-    </h3>
-      <img v-if="
-                imgSource !==
-                'https://www.billboard.com/wp-content/themes/vip/pmc-billboard-2021/assets/public/lazyload-fallback.gif'
-              " :src="imgSource" class="m-6 rounded-lg shadow-navy shadow-lg max-h-[12rem] mt-9 " />
+      <h3
+        v-if="rank && rank < 10"
+        class="text-green text-4xl tracking-wider font-jetbrains italic absolute"
+      >
+        0{{ rank }}.
+      </h3>
+      <h3
+        v-else
+        class="text-green text-4xl tracking-wider font-jetbrains italic absolute"
+      >
+        {{ rank }}.
+      </h3>
+      <img
+        v-if="
+          imgSource !==
+          'https://www.billboard.com/wp-content/themes/vip/pmc-billboard-2021/assets/public/lazyload-fallback.gif'
+        "
+        :src="imgSource"
+        class="m-6 rounded-lg shadow-navy shadow-lg h-[12rem] mt-9"
+      />
       <img v-else src="https://static.thenounproject.com/png/3580649-200.png" />
     </div>
-    <div id="info-text" class="flex flex-col text-left font-karla ml-6 mt-6 col-span-2">
+    <div
+      id="info-text"
+      class="flex flex-col text-left font-karla ml-6 mt-6 col-span-2"
+    >
       <h3
         v-if="artist"
         class="font-bold text-xl text-lightest-slate tracking-wider"
